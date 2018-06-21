67764bba4e8835a3f40e973d7f258064c382fc8d
GaopanHuang <huanggpan@163.com>
2018-06-21 20:03:06 +0800

    modify lstmseq

--- lstmseq.py
+++ lstmseq.py
@@ -1,6 +1,6 @@
 import os
 #gpu_id = '1,2'
-gpu_id = '3'
+gpu_id = '2'
 os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_id)
 os.system('echo $CUDA_VISIBLE_DEVICES')
 
@@ -56,7 +56,7 @@ print ('building model...')
 batch_size = 128  # Batch size for training.
 epochs = 100  # Number of epochs to train for.
 train_num = int(0.9*len(logstrs))
-val_num = int(0.1*len(logstrs)-2000)
+val_num = int(0.1*len(logstrs)-3000)
 
 encoder_inputs = Input(shape=(None, log_dim))
 #encoder = LSTM(1024, return_state=True, return_sequences=True)(encoder_inputs)
@@ -75,14 +75,12 @@ x = Reshape((1024, 2))(encoder_states)
 x = Conv1D(64, 5, strides=2, padding='valid')(x)
 x = BatchNormalization()(x)
 x = Activation('relu')(x)
-#print (x.get_shape())
-#x = Dropout(0.2)(x)
-#print (x.get_shape())
+x = Dropout(0.2)(x)
 
 x = Conv1D(64, 5, strides=4, padding='valid')(x)
+x = BatchNormalization()(x)
 x = Activation('relu')(x)
-#x = Dropout(0.2)(x)
-#print (x.get_shape())
+x = Dropout(0.2)(x)
 
 #x = Conv1D(32, 5, strides=2, padding='valid')(x)
 #x = BatchNormalization()(x)
@@ -96,16 +94,15 @@ x = Activation('relu')(x)
 
 x = GlobalMaxPooling1D()(x)
 
-#print (x.get_shape())
 # and a logistic layer -- let's say we have 2 classes
 predictions = Dense(2, activation='softmax')(x)
 
 model = Model(encoder_inputs, predictions)
 model.compile(optimizer='Nadam', metrics=['accuracy'],loss='binary_crossentropy')
 
-def generate_train(batch_size):
+def generate_data(batch_size,num, index):
     while True:
-        steps = int(train_num/batch_size)
+        steps = int(num/batch_size)
         for i in range(steps):
             x = np.zeros(
                     (batch_size, timestep, log_dim),
@@ -114,8 +111,7 @@ def generate_train(batch_size):
                     (batch_size, 2),
                     dtype='float32')
             for m in range(batch_size):
-                log_idx = np.random.randint(0,train_num)
-                #log_idx = i*batch_size+m
+                log_idx = np.random.randint(index,num+index)
                 logstr = logstrs[log_idx]
                 x[m, 0, log_indices[logstr]] = 1.
                 for j in range(log_idx+1, len(logstrs)):
@@ -138,41 +134,6 @@ def generate_train(batch_size):
 #            print ('\n              err: %.4f' % (1.*np.sum(y[:,1])/len(y)))
             yield (x, y)
 
-def generate_val(batch_size):
-    while True:
-        steps = int(val_num/batch_size)
-        for i in range(steps):
-            x = np.zeros(
-                    (batch_size, timestep, log_dim),
-                    dtype='float32')
-            y = np.zeros(
-                    (batch_size, 2),
-                    dtype='float32')
-            for m in range(batch_size):
-                log_idx = i*batch_size+m+train_num
-                logstr = logstrs[log_idx]
-                x[m, 0, log_indices[logstr]] = 1.
-                for j in range(log_idx+1, len(logstrs)):
-                    if abstime[j]-abstime[log_idx]<time_len:
-                        logstr = logstrs[j]
-                        if (j-log_idx)>(timestep-1):
-                            break
-                        x[m, j-log_idx, log_indices[logstr]] = 1.
-                    else:
-                        break
-                tmp_str = ''
-                for k in range(j+1, len(logstrs)):
-                    if abstime[k]-abstime[j]<time_future:
-                        tmp_str += logstrs[k]
-                    else:
-                        break
-                if tmp_str.upper().find('ERROR')!=-1:
-                    y[m, 1] = 1.
-                y[m,0] = 1-y[m,1]
-
-            yield (x, y)
-
-
 print ('training...')
 from keras.callbacks import TensorBoard, CSVLogger,EarlyStopping
 class BatchPerf(keras.callbacks.Callback):
@@ -194,65 +155,63 @@ tensorboard = TensorBoard(log_dir='./results/logs')
 csv_logger = CSVLogger('./results/epoch_perf.log')
 es = EarlyStopping(monitor='val_loss', mode='auto',patience=6)
 
-his = model.fit_generator(generator=generate_train(batch_size),
+his = model.fit_generator(generator=generate_data(batch_size, train_num, 0),
         steps_per_epoch=int(train_num/batch_size),
         epochs=epochs,
         callbacks=[tensorboard,csv_logger,batPerf,es],
-        validation_data=generate_val(batch_size),
+        validation_data=generate_data(batch_size, val_num, train_num),
         validation_steps=int(val_num/batch_size))
-#his = model.fit(x_train, y_train,
-#        batch_size=batch_size,
-#        epochs=epochs,
-#        validation_split=0.1,
-#        callbacks=[tensorboard,csv_logger,batPerf, es],)
 
 loss_df = pd.DataFrame([batPerf.losses, batPerf.acc]).T
 loss_df.to_csv('./results/batch_perf.log',header=['loss', 'acc'],index=None)
 
-print("Saved model architecture")
-model_json = model.to_json()
-with open("model_arch.json", "w") as json_file:
-    json_file.write(model_json)
+#print("Saved model architecture")
+#model_json = model.to_json()
+#with open("model_arch_lstm1.json", "w") as json_file:
+#    json_file.write(model_json)
 # serialize weights to HDF5, saves the weights of the model as a HDF5 file.
-print("Saved model weights")
-model.save_weights("model.h5")
+#print("Saved model weights")
+#model.save_weights("model_lstm1.h5")
 
 
 test_num = 1000
 test_x = np.zeros(
     (test_num, timestep, log_dim),
     dtype='float32')
-text_y = np.zeros(
+test_y = np.zeros(
     (test_num, 2),
     dtype='float32')
 
-for i in range(train_num+val_num, train_num+val_num+test_num):
-    test_idx = i-train_num+val_num
-    logstr = logstrs[i]
-    test_x[test_idx, 0, log_indices[logstr]] = 1.
-    for j in range(i+1, len(logstrs)):
-        if abstime[j]-abstime[i]<time_len:
+for m in range(test_num):
+    log_idx = np.random.randint(0,len(logstrs)-200)
+    logstr = logstrs[log_idx]
+    test_x[m, 0, log_indices[logstr]] = 1.
+    for j in range(log_idx+1, len(logstrs)):
+        if abstime[j]-abstime[log_idx]<time_len:
             logstr = logstrs[j]
-            if (j-i)>(timestep-1):
+            if (j-log_idx)>(timestep-1):
                 break
-            test_x[text_idx, j-i, log_indices[logstr]] = 1.
+            test_x[m, j-log_idx, log_indices[logstr]] = 1.
         else:
             break
     tmp_str = ''
     for k in range(j+1, len(logstrs)):
+        if k>=len(logstrs):
+            break
         if abstime[k]-abstime[j]<time_future:
             tmp_str += logstrs[k]
         else:
             break
     if tmp_str.upper().find('ERROR')!=-1:
-        y[m, 1] = 1.
-    y[m,0] = 1-y[m,1]
+        test_y[m, 1] = 1.
+    test_y[m,0] = 1-test_y[m,1]
+print ('\nerr: %.4f' % (1.*np.sum(test_y[:,1])/len(test_y)))
 
 y_ = model.predict(test_x)
 print ('true:')
-print (text_y[:50])
+print (test_y[:20])
 print ('predict:')
-print (y_[:50])
+print (y_[:20])
 y_pred = np.zeros(y_.shape, dtype='float32')
 for i in range(len(y_)):
     if y_[i,0]>y_[i,1]:
@@ -260,6 +219,6 @@ for i in range(len(y_)):
     else:
         y_pred[i,1] = 1.
     
-acc = np.mean(np.equal(y_pred, text_y))
-logloss = log_loss(text_y,y_)
+acc = np.mean(np.equal(y_pred, test_y))
+logloss = log_loss(test_y,y_)
 print ('acc:%f; logloss:%f' % (acc, logloss))
